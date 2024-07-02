a06c2e520401bccfc78ec0563f6228a801cf1dd7
Vinh Nguyen <argh2xxx@gmail.com>
2024-07-02 17:13:04 -0400

    First commit.

---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,5 @@
+const ProfilePage = () => {
+  return <div className="page">Profile Page</div>;
+};
+
+export default ProfilePage;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import Link from "next/link";
+import MobileMenu from "./MobileMenu";
+import Image from "next/image";
+import { ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
+
+const Navbar = () => {
+  return (
+    <div className="h-24 flex items-center justify-between">
+      {/*LEFT*/}
+      <div className="md:hidden lg:block w-[20%]">
+        <Link href="/" className="font-bold text-xl text-blue-700">
+          Jiivee
+        </Link>
+      </div>
+      {/*CENTER*/}
+      <div className="hidden md:flex w-[50%] text-sm">
+        {/*LINKS*/}
+        <div className="flex gap-6 text-gray-600">
+          <Link href="/" className="flex items-center gap-2">
+            <Image src="/home.png" alt="Homepage" width={16} height={16} className="w-4 h-4" />
+            <span>Homepage</span>
+          </Link>
+          <Link href="/" className="flex items-center gap-2">
+            <Image src="/friends.png" alt="Friends" width={16} height={16} className="w-4 h-4" />
+            <span>Friends</span>
+          </Link>
+          <Link href="/" className="flex items-center gap-2">
+            <Image src="/stories.png" alt="Stories" width={16} height={16} className="w-4 h-4" />
+            <span>Stories</span>
+          </Link>
+        </div>
+      </div>
+      {/*RIGHT*/}
+      <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
+        <ClerkLoading>
+          <div className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white" role="status">
+            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
+          </div>
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <div className="cursor-pointer">
+              <Image src="/people.png" width={20} height={20} alt="" />
+            </div>
+            <div className="cursor-pointer">
+              <Image src="/messages.png" width={20} height={20} alt="" />
+            </div>
+            <div className="cursor-pointer">
+              <Image src="/notifications.png" width={20} height={20} alt="" />
+            </div>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <div className="flex items-center gap-2 text-sm">
+              <Image src="/login.png" width={20} height={20} alt="" />
+              <Link href="/sign-in">Login/Register</Link>
+            </div>
+          </SignedOut>
+        </ClerkLoaded>
+        <MobileMenu />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
