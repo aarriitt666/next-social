9c1503963ca90c2900531cb39d6646d53c6b8822
Vinh Nguyen <argh2xxx@gmail.com>
2024-07-20 14:19:51 -0400

    Work on story.

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,63 +1,37 @@
-import Image from "next/image";
-const Stories = () => {
+import prisma from "@/lib/client";
+import { auth } from "@clerk/nextjs/server";
+import StoryList from "./StoryList";
+const Stories = async () => {
+  const { userId: currentUserId } = auth();
+  if (!currentUserId) return null;
+  const stories = await prisma.story.findMany({
+    where: {
+      expiresAt: {
+        gt: new Date(),
+      },
+      OR: [
+        {
+          user: {
+            followers: {
+              some: {
+                followerId: currentUserId,
+              },
+            },
+          },
+        },
+        {
+          userId: currentUserId,
+        },
+      ],
+    },
+    include: {
+      user: true,
+    },
+  });
   return (
     <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs scrollbar-hide">
       <div className="flex gap-8 w-max">
-        {/* STORY */}
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image src="https://images.pexels.com/photos/24461004/pexels-photo-24461004/free-photo-of-model-in-long-black-dress.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-          <span className="font-medium">Ricky</span>
-        </div>
-        {/* STORY */}
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image src="https://images.pexels.com/photos/24461004/pexels-photo-24461004/free-photo-of-model-in-long-black-dress.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-          <span className="font-medium">Ricky</span>
-        </div>
-        {/* STORY */}
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image src="https://images.pexels.com/photos/24461004/pexels-photo-24461004/free-photo-of-model-in-long-black-dress.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-          <span className="font-medium">Ricky</span>
-        </div>
-        {/* STORY */}
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image src="https://images.pexels.com/photos/24461004/pexels-photo-24461004/free-photo-of-model-in-long-black-dress.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-          <span className="font-medium">Ricky</span>
-        </div>
-        {/* STORY */}
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image src="https://images.pexels.com/photos/24461004/pexels-photo-24461004/free-photo-of-model-in-long-black-dress.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-          <span className="font-medium">Ricky</span>
-        </div>
-        {/* STORY */}
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image src="https://images.pexels.com/photos/24461004/pexels-photo-24461004/free-photo-of-model-in-long-black-dress.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-          <span className="font-medium">Ricky</span>
-        </div>
-        {/* STORY */}
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image src="https://images.pexels.com/photos/24461004/pexels-photo-24461004/free-photo-of-model-in-long-black-dress.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-          <span className="font-medium">Ricky</span>
-        </div>
-        {/* STORY */}
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image src="https://images.pexels.com/photos/24461004/pexels-photo-24461004/free-photo-of-model-in-long-black-dress.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-          <span className="font-medium">Ricky</span>
-        </div>
-        {/* STORY */}
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image src="https://images.pexels.com/photos/24461004/pexels-photo-24461004/free-photo-of-model-in-long-black-dress.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-          <span className="font-medium">Ricky</span>
-        </div>
-        {/* STORY */}
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image src="https://images.pexels.com/photos/24461004/pexels-photo-24461004/free-photo-of-model-in-long-black-dress.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-          <span className="font-medium">Ricky</span>
-        </div>
-        {/* STORY */}
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image src="https://images.pexels.com/photos/24461004/pexels-photo-24461004/free-photo-of-model-in-long-black-dress.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-          <span className="font-medium">Ricky</span>
-        </div>
+        <StoryList stories={stories} userId={currentUserId} />
       </div>
     </div>
   );
--- src/lib/action.ts
+++ src/lib/action.ts
@@ -296,3 +296,37 @@ export const addPost = async (formData:FormData, img:string) => {
     throw new Error("Something went wrong!");
   }
 }
+
+export const addStory = async (img:string) => {
+
+  const {userId} = auth();
+  if (!userId) throw new Error("User is not authenticated!");
+  try {
+    const existingStory = await prisma.story.findFirst({
+      where: {
+        userId,
+      },
+    });
+    if (existingStory) {
+      await prisma.story.delete({
+        where: {
+          id: existingStory.id,
+        },
+      })
+    }
+    const createdStory = await prisma.story.create({
+      data:{
+        img,
+        userId,
+        expiresAt: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      },
+      include:{
+        user: true,
+      }
+    });
+    return createdStory;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Something went wrong!");
+  }
+}
